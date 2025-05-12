d4347a624d7fc1e0fa70864da658fd87cee87016
Eero Saarro <saarro.eero@gmail.com>
2025-05-12 15:18:22 +0300

    Added for loop to process each field in the DataFrame, implemented main() function, and stripped whitespace from column headers

--- code/main.py
+++ code/main.py
@@ -6,25 +6,45 @@ from selenium.webdriver.common.keys import Keys
 import os
 import time
 
-# Open web browser and navigate to the website
-driver = webdriver.Chrome()
-driver.get("https://rpachallenge.com/")
-print(driver.title)
 
-# Find the "Start" button and click it
-start_button = driver.find_element(By.XPATH, '//button[text()="Start"]')
-start_button.click()
+def main():
+    # Load the CSV file
+    df = pd.read_excel('challenge.xlsx')
+    df.columns = df.columns.str.strip()
+    print(df.head())
+    print(df.shape) # Shape of the DataFrame 10,7 so 10 rows:
+    
+    # Open web browser and navigate to the website
+    driver = webdriver.Chrome()
+    driver.get("https://rpachallenge.com/")
 
-# Find the column headers and fill the input fields
-#input1 = driver.find_element(By.XPATH, '//input[@ng-reflect-name="labelAddress"]')
-input1 = driver.find_element(By.XPATH, '//label[text()="Address"]//following-sibling::input')
-input1.send_keys("Test address")
-
-time.sleep(5)
-driver.quit()
+    # Find the "Start" button and click it
+    start_button = driver.find_element(By.XPATH, '//button[text()="Start"]')
+    start_button.click()
 
 
-# Load the CSV file
-df = pd.read_excel('challenge.xlsx')
-print(df.head())
 
+    # Loop through the rows of the DataFrame
+    for index, row in df.iterrows():
+        print(f"Filling row {index + 1}")
+        for column, value in row.items():
+            print(f"Filling {column} with {value}")
+
+            # Find the input field for the current column
+            input_field = driver.find_element(By.XPATH, f'//label[text()="{column}"]//following-sibling::input')
+            
+            # Clear the input field and enter the value
+            input_field.clear()
+            input_field.send_keys(value)
+
+        # Press the submit button
+        submit_button = driver.find_element(By.XPATH, '//input[@value="Submit"]')
+        submit_button.click()
+
+    time.sleep(5)
+    driver.quit()
+
+
+
+if __name__ == "__main__":
+    main()
