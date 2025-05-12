69f84f4f45962b49e08e53d6854bd58c1c1bbc48
Eero Saarro <saarro.eero@gmail.com>
2025-05-12 14:36:15 +0300

    learning to use XPATH find element

--- code/main.py
+++ code/main.py
@@ -6,9 +6,15 @@ from selenium.webdriver.common.keys import Keys
 import os
 import time
 
+# Open web browser and navigate to the website
 driver = webdriver.Chrome()
-driver.get("https://www.python.org")
+driver.get("https://rpachallenge.com/")
 print(driver.title)
+
+# Find the "Start" button and click it
+start_button = driver.find_element(By.XPATH, '//button[text()="Start"]')
+start_button.click()
+
 time.sleep(5)
 driver.quit()
 
