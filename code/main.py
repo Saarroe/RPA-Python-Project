bc01f3822715ce23a46bff2e78810bd7d98a807e
Eero Saarro <saarro.eero@gmail.com>
2025-05-12 15:00:38 +0300

    Find input fields and labels

--- code/main.py
+++ code/main.py
@@ -15,6 +15,11 @@ print(driver.title)
 start_button = driver.find_element(By.XPATH, '//button[text()="Start"]')
 start_button.click()
 
+# Find the column headers and fill the input fields
+#input1 = driver.find_element(By.XPATH, '//input[@ng-reflect-name="labelAddress"]')
+input1 = driver.find_element(By.XPATH, '//label[text()="Address"]//following-sibling::input')
+input1.send_keys("Test address")
+
 time.sleep(5)
 driver.quit()
 
